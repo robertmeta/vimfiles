667551b2e26ad1afeaebfb66d0d1151af1b24f96
Robert Melton <robert@robertmelton.com>
2014-08-08 03:38:15 -0400

    SAVEPOINT

--- get-plugins.zsh
+++ get-plugins.zsh
@@ -1,24 +1,24 @@
 rm -rf bundle
 mkdir -p bundle
 
-#git clone https://github.com/airblade/vim-gitgutter.git bundle/gitgutter && rm -rf bundle/gitgutter/.git
 git clone https://github.com/christoomey/vim-tmux-navigator bundle/vim-tmux-navigator && rm -rf bundle/vim-tmux-navigator/.git
-#git clone https://github.com/ciaranm/inkpot.git bundle/inkpot && rm -rf bundle/inkpot/.git
-#git clone https://github.com/dgryski/vim-godef bundle/godef && rm -rf bundle/godef/.git
+git clone https://github.com/ciaranm/inkpot.git bundle/inkpot && rm -rf bundle/inkpot/.git
 git clone https://github.com/ervandew/supertab.git bundle/supertab && rm -rf bundle/supertab/.git
 git clone https://github.com/fatih/vim-go.git bundle/vim-go && rm -rf bundle/vim-go/.git
-#git clone https://github.com/itchyny/landscape.vim.git bundle/landscape && rm -rf bundle/landscape/.git
+git clone https://github.com/itchyny/landscape.vim.git bundle/landscape && rm -rf bundle/landscape/.git
 git clone https://github.com/junegunn/seoul256.vim bundle/seoul256 && rm -rf bundle/seoul256/.git
 git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp && rm -rf bundle/ctrlp/.git
 git clone https://github.com/kien/rainbow_parentheses.vim bundle/rainbow_parentheses && rm -rf bundle/rainbow_parentheses/.git
 git clone https://github.com/leshill/vim-json.git bundle/json && rm -rf bundle/json/.git
 git clone https://github.com/Lokaltog/vim-easymotion bundle/easymotion && rm -rf bundle/easymotion/.git
-#git clone https://github.com/paradigm/TextObjectify.git bundle/textobjectify && rm -rf bundle/textobjectify/.git
+git clone https://github.com/majutsushi/tagbar bundle/tagbar && rm -rf bundle/tagbar/.git
+git clone https://github.com/paradigm/TextObjectify.git bundle/textobjectify && rm -rf bundle/textobjectify/.git
 git clone https://github.com/PProvost/vim-ps1 bundle/ps1 && rm -rf bundle/ps1/.git
+git clone https://github.com/ricardovaleriano/vim-github-theme bundle/githubtheme && rm -rf bundle/githubtheme/.git
 git clone https://github.com/rking/ag.vim bundle/ag && rm -rf bundle/ag/.git
 git clone https://github.com/scrooloose/nerdtree bundle/nerdtree && rm -rf bundle/nerdtree/.git
 git clone https://github.com/scrooloose/syntastic bundle/syntastic && rm -rf bundle/syntastic/.git
-#git clone https://github.com/tomasr/molokai.git bundle/molokai && rm -rf bundle/molokai/.git
+git clone https://github.com/tomasr/molokai.git bundle/molokai && rm -rf bundle/molokai/.git
 git clone https://github.com/tpope/vim-abolish bundle/abolish && rm -rf bundle/abolish/.git
 git clone https://github.com/tpope/vim-commentary bundle/commentary && rm -rf bundle/commentary/.git
 git clone https://github.com/tpope/vim-dispatch bundle/dispatch && rm -rf bundle/dispatch/.git
@@ -31,4 +31,5 @@ git clone https://github.com/tpope/vim-surround bundle/surround && rm -rf bundle
 git clone https://github.com/tpope/vim-unimpaired bundle/unimpaired && rm -rf bundle/unimpaired/.git
 git clone https://github.com/tpope/vim-vinegar bundle/vinegar && rm -rf bundle/vinegar/.git
 git clone https://github.com/wellle/targets.vim bundle/targets && rm -rf bundle/targets/.git
-git clone https://github.com/majutsushi/tagbar bundle/tagbar && rm -rf bundle/tagbar/.git
+git clone https://github.com/nanotech/jellybeans.vim bundle/jellybeans && rm -rf bundle/jellybeans/.git
+git clone https://github.com/tpope/vim-vividchalk bundle/vividchalk && rm -rf bundle/vividchalk/.git
