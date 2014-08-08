7c2b5ee8890cd3d5c158bc1c085ee0e4b39849d8
Robert Melton <robert@robertmelton.com>
2014-08-08 05:23:14 -0400

    SAVEPOINT

--- get-plugins.zsh
+++ get-plugins.zsh
@@ -1,6 +1,7 @@
 rm -rf bundle
 mkdir -p bundle
 
+git clone https://github.com/altercation/vim-colors-solarized bundle/solarized && rm -rf bundle/solarized/.git
 git clone https://github.com/christoomey/vim-tmux-navigator bundle/vim-tmux-navigator && rm -rf bundle/vim-tmux-navigator/.git
 git clone https://github.com/ciaranm/inkpot.git bundle/inkpot && rm -rf bundle/inkpot/.git
 git clone https://github.com/ervandew/supertab.git bundle/supertab && rm -rf bundle/supertab/.git
@@ -12,6 +13,7 @@ git clone https://github.com/kien/rainbow_parentheses.vim bundle/rainbow_parenth
 git clone https://github.com/leshill/vim-json.git bundle/json && rm -rf bundle/json/.git
 git clone https://github.com/Lokaltog/vim-easymotion bundle/easymotion && rm -rf bundle/easymotion/.git
 git clone https://github.com/majutsushi/tagbar bundle/tagbar && rm -rf bundle/tagbar/.git
+git clone https://github.com/nanotech/jellybeans.vim bundle/jellybeans && rm -rf bundle/jellybeans/.git
 git clone https://github.com/paradigm/TextObjectify.git bundle/textobjectify && rm -rf bundle/textobjectify/.git
 git clone https://github.com/PProvost/vim-ps1 bundle/ps1 && rm -rf bundle/ps1/.git
 git clone https://github.com/ricardovaleriano/vim-github-theme bundle/githubtheme && rm -rf bundle/githubtheme/.git
@@ -30,8 +32,5 @@ git clone https://github.com/tpope/vim-speeddating bundle/speeddating && rm -rf 
 git clone https://github.com/tpope/vim-surround bundle/surround && rm -rf bundle/surround/.git
 git clone https://github.com/tpope/vim-unimpaired bundle/unimpaired && rm -rf bundle/unimpaired/.git
 git clone https://github.com/tpope/vim-vinegar bundle/vinegar && rm -rf bundle/vinegar/.git
-git clone https://github.com/wellle/targets.vim bundle/targets && rm -rf bundle/targets/.git
-git clone https://github.com/nanotech/jellybeans.vim bundle/jellybeans && rm -rf bundle/jellybeans/.git
 git clone https://github.com/tpope/vim-vividchalk bundle/vividchalk && rm -rf bundle/vividchalk/.git
-git clone https://github.com/altercation/vim-colors-solarized bundle/solarized && rm -rf bundle/solarized/.git
-
+git clone https://github.com/wellle/targets.vim bundle/targets && rm -rf bundle/targets/.git
