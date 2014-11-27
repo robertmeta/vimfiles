6c637a80aa0cb4a903292693832ae9132e1dc73f
Robert Melton <robert@robertmelton.com>
2014-11-27 05:44:24 -0600

    WIP

--- get-plugins.zsh
+++ get-plugins.zsh
@@ -3,20 +3,20 @@ mkdir -p bundle
 
 # Themes
 git clone https://github.com/junegunn/seoul256.vim bundle/seoul256 && rm -rf bundle/seoul256/.git
-git clone https://github.com/tomasr/molokai.git bundle/molokai && rm -rf bundle/molokai/.git
+git clone https://github.com/tomasr/molokai bundle/molokai && rm -rf bundle/molokai/.git
 
 # Go 
-git clone https://github.com/fatih/vim-go.git bundle/vim-go && rm -rf bundle/vim-go/.git
+git clone https://github.com/fatih/vim-go bundle/vim-go && rm -rf bundle/vim-go/.git
 
 # Tmux
 git clone https://github.com/christoomey/vim-tmux-navigator bundle/vim-tmux-navigator && rm -rf bundle/vim-tmux-navigator/.git
-#git clone https://github.com/benmills/vimux.git bundle/vimux && rm -rf bundle/vimux/.git
-#git clone https://github.com/benmills/vimux-golang.git bundle/vimux-golang && rm -rf bundle/vimux-golang/.git
+#git clone https://github.com/benmills/vimux bundle/vimux && rm -rf bundle/vimux/.git
+#git clone https://github.com/benmills/vimux-golang bundle/vimux-golang && rm -rf bundle/vimux-golang/.git
 #git clone https://github.com/tpope/vim-dispatch bundle/dispatch && rm -rf bundle/dispatch/.git
 
 # Rainbow Parens
 git clone https://github.com/kien/rainbow_parentheses.vim bundle/rainbow_parentheses && rm -rf bundle/rainbow_parentheses/.git
-#git clone https://github.com/amdt/vim-niji.git bundle/niji && rm -rf bundle/niji/.git
+#git clone https://github.com/amdt/vim-niji bundle/niji && rm -rf bundle/niji/.git
 
 # General
 git clone https://github.com/rking/ag.vim bundle/ag && rm -rf bundle/ag/.git
@@ -33,42 +33,43 @@ git clone https://github.com/tpope/vim-speeddating bundle/speeddating && rm -rf 
 git clone https://github.com/tpope/vim-surround bundle/surround && rm -rf bundle/surround/.git
 
 # Improved motions
-git clone https://github.com/ervandew/supertab.git bundle/supertab && rm -rf bundle/supertab/.git
-git clone https://github.com/dahu/vim-fanfingtastic.git bundle/fanfingtastic && rm -rf bundle/fanfingtastic/.git
+git clone https://github.com/ervandew/supertab bundle/supertab && rm -rf bundle/supertab/.git
+git clone https://github.com/dahu/vim-fanfingtastic bundle/fanfingtastic && rm -rf bundle/fanfingtastic/.git
 git clone https://github.com/wellle/targets.vim bundle/targets && rm -rf bundle/targets/.git
-#git clone https://github.com/paradigm/TextObjectify.git bundle/textobjectify && rm -rf bundle/textobjectify/.git
+#git clone https://github.com/paradigm/TextObjectify bundle/textobjectify && rm -rf bundle/textobjectify/.git
 
 # Visual
 git clone https://github.com/majutsushi/tagbar bundle/tagbar && rm -rf bundle/tagbar/.git
 git clone https://github.com/scrooloose/nerdtree bundle/nerdtree && rm -rf bundle/nerdtree/.git
 git clone https://github.com/scrooloose/syntastic bundle/syntastic && rm -rf bundle/syntastic/.git
 git clone https://github.com/Lokaltog/vim-easymotion bundle/easymotion && rm -rf bundle/easymotion/.git
-git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp && rm -rf bundle/ctrlp/.git
+git clone https://github.com/kien/ctrlp.vim bundle/ctrlp && rm -rf bundle/ctrlp/.git
 
 # PowerShell
 git clone https://github.com/PProvost/vim-ps1 bundle/ps1 && rm -rf bundle/ps1/.git
 
 # Angular support
-git clone https://github.com/burnettk/vim-angular.git bundle/angular && rm -rf bundle/angular/.git
-git clone https://github.com/claco/jasmine.vim.git bundle/jasmine && rm -rf bundle/jasmine/.git
-git clone https://github.com/othree/javascript-libraries-syntax.vim.git bundle/javascriptlibsyntax && rm -rf bundle/javascriptlibsyntax/.git
-git clone https://github.com/pangloss/vim-javascript.git bundle/javascript && rm -rf bundle/javascript/.git
+git clone https://github.com/burnettk/vim-angular bundle/angular && rm -rf bundle/angular/.git
+git clone https://github.com/claco/jasmine.vim bundle/jasmine && rm -rf bundle/jasmine/.git
+git clone https://github.com/othree/javascript-libraries-syntax.vim bundle/javascriptlibsyntax && rm -rf bundle/javascriptlibsyntax/.git
+git clone https://github.com/pangloss/vim-javascript bundle/javascript && rm -rf bundle/javascript/.git
 
 # Json
-git clone https://github.com/leshill/vim-json.git bundle/json && rm -rf bundle/json/.git
+git clone https://github.com/leshill/vim-json bundle/json && rm -rf bundle/json/.git
 
 # Html5
-git clone https://github.com/othree/html5.vim.git bundle/html5 && rm -rf bundle/html5/.git
+git clone https://github.com/othree/html5.vim bundle/html5 && rm -rf bundle/html5/.git
 
 # JS
-git clone https://github.com/marijnh/tern_for_vim.git bundle/ternforvim && rm -rf bundle/ternforvim/.git
-git clone https://github.com/mephux/vim-jsfmt.git bundle/jsfmt && rm -rf bundle/jsfmt/.git
+git clone https://github.com/marijnh/tern_for_vim bundle/ternforvim && rm -rf bundle/ternforvim/.git
+git clone https://github.com/mephux/vim-jsfmt bundle/jsfmt && rm -rf bundle/jsfmt/.git
 
 # Themes
 git clone https://github.com/altercation/vim-colors-solarized bundle/solarized && rm -rf bundle/solarized/.git
 git clone https://github.com/freeo/vim-kalisi bundle/kalisi && rm -rf bundle/kalisi/.git
-git clone https://github.com/itchyny/landscape.vim.git bundle/landscape && rm -rf bundle/landscape/.git
+git clone https://github.com/itchyny/landscape.vim bundle/landscape && rm -rf bundle/landscape/.git
 git clone https://github.com/nanotech/jellybeans.vim bundle/jellybeans && rm -rf bundle/jellybeans/.git
-git clone https://github.com/ricardovaleriano/vim-github-theme bundle/githubtheme && rm -rf bundle/githubtheme/.git
+git clone https://github.com/ricardovaleriano/vim-github-theme bundle/github && rm -rf bundle/github/.git
 git clone https://github.com/tpope/vim-vividchalk bundle/vividchalk && rm -rf bundle/vividchalk/.git
-git clone https://github.com/ciaranm/inkpot.git bundle/inkpot && rm -rf bundle/inkpot/.git
+git clone https://github.com/ciaranm/inkpot bundle/inkpot && rm -rf bundle/inkpot/.git
+git clone https://github.com/zenorocha/dracula-theme bundle/dracula && rm -rf bundle/dracula/.git
