c2d5e158abde122be07608b61c45d9fe8153edfc
Robert Melton <robert@robertmelton.com>
2015-06-08 18:22:57 -0500

    WIP

--- get-plugins.zsh
+++ get-plugins.zsh
@@ -2,8 +2,19 @@ rm -rf bundle
 mkdir -p bundle
 
 # Themes {{{
+git clone https://github.com/ciaranm/inkpot bundle/inkpot && rm -rf bundle/inkpot/.git
+git clone https://github.com/freeo/vim-kalisi bundle/kalisi && rm -rf bundle/kalisi/.git
 git clone https://github.com/jonathanfilip/vim-lucius bundle/lucius && rm -rf bundle/lucius/.git
+git clone https://github.com/junegunn/seoul256.vim bundle/seoul256 && rm -rf bundle/seoul256/.git
 git clone https://github.com/NLKNguyen/papercolor-theme.git bundle/paper && rm -rf bundle/paper/.git
+git clone https://github.com/vim-scripts/blackdust.vim bundle/blackdust && rm -rf bundle/blackdust/.git
+git clone https://github.com/nanotech/jellybeans.vim bundle/jellybeans && rm -rf bundle/jellybeans/.git
+git clone https://github.com/romainl/Apprentice bundle/apprentice && rm -rf bundle/apprentice/.git
+git clone https://github.com/romainl/Disciple bundle/disciple && rm -rf bundle/disciple/.git
+git clone https://github.com/tomasr/molokai bundle/molokai && rm -rf bundle/molokai/.git
+git clone https://github.com/tpope/vim-vividchalk bundle/vividchalk && rm -rf bundle/vividchalk/.git
+mkdir -p bundle/herald/colors
+curl https://h3rald.com/files/herald.vim > bundle/herald/colors/herald.vim
 # }}}
 
 # Gutentags is a plugin that takes care of the much needed management of tags
